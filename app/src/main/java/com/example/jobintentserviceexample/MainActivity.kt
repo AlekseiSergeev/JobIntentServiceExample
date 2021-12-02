d26a36617726e24008c48ea259dead6c130496a6
AlekseiSergeev <mrx45ru@gmail.com>
2021-12-02 12:07:44 +0500

    Add new project

---
+++ app/src/main/java/com/example/jobintentserviceexample/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.jobintentserviceexample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.jobintentserviceexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.workButton.setOnClickListener {
+            val input = binding.editTextInput.text.toString()
+
+            val serviceIntent = Intent(this, ExampleJobIntentService::class.java)
+            serviceIntent.putExtra("inputExtra", input)
+
+            ExampleJobIntentService().enqueueWork(this, serviceIntent)
+        }
+    }
+}
